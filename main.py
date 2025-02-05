56d577fb6245aecb5aea3a42ffa93048c26ee3dd
inspektral <titoscutari@gmail.com>
2025-02-05 01:34:59 +0100

    analysis done

--- main.py
+++ main.py
@@ -1,30 +1,38 @@
 import loader
 from analyzer import Analyzer
 import json
+import pandas as pd
+import tqdm
+
+NEW_DATASET = False
+ANALYSIS_RESULTS = 'results.csv'
 
 def main():
-    config = loader.load_config(True)
-    df = loader.init_dataset(config, True)
-    
+    if NEW_DATASET:
+        config = loader.load_config(True)
+        df = loader.init_dataset(config, True)
+    else:
+        df = pd.read_csv(ANALYSIS_RESULTS)
+
     analyzer = Analyzer()
     
-    for index, row in df.iterrows():
+    for index, row in tqdm.tqdm(df.iterrows(), total=len(df), desc="Analyzing files"):
 
-        print(f"Analyzing file: {row['path']}")
-        results = analyzer.analyze(row['path'])
+        if 'analyzed' in df.columns and row['analyzed'] == True:
+            print(f"File already analyzed: {row['path']}")
         
-        # print(f"Results: {results}")
-        for key in results:
-            if isinstance(results[key], list):
-                df.loc[index, key] = json.dumps(results[key])
-            else:
-                df.loc[index, key] = results[key]
+        else:
+            print(f"Analyzing file: {row['path']}")
+            results = analyzer.analyze(row['path'])
+            
+            for key in results:
+                if isinstance(results[key], list):
+                    df.loc[index, key] = json.dumps(results[key])
+                else:
+                    df.loc[index, key] = results[key]
 
 
-        if index > 10:
-            break
-
-    df.to_csv('results.csv', index=False)
+            df.to_csv(ANALYSIS_RESULTS, index=False)
 
 if __name__ == '__main__':
     main()
